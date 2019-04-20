1831988d60c80daa5e395340d3b4b773fb7a8a98
jerry <yx1900@outlook.com>
2019-04-20 10:54:36 +0800

    模板

---
+++ C_1/test1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void fun4()
+{
+     ///。。。。
+}
+void fun3()
+{
+     ///。。。
+}
+void fun2()
+{
+     //。。。
+}
+void fun1()
+{
+     int a;//aaaa
+}
+int main(int argc, char *argv[])
+{
+     // fun1();
+     // fun2();
+     // fun3();
+     
+
+     fun4();
+
+     printf("\n");
+     return 0;
+}
